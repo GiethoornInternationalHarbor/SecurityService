1eed38e41b7765ee2c45dc2dae4b2244aee12bf5
Marco <marcohavermans+github@hotmail.nl>
2018-05-05 15:03:07 +0200

    Added initial migration of database.

--- SecurityService.Core/Models/Product.cs
+++ SecurityService.Core/Models/Product.cs
@@ -1,7 +1,10 @@
-﻿namespace SecurityService.Core.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SecurityService.Core.Models
 {
 	public class Product
 	{
+		[Key]
 		public string Name { get; set; }
 		public string Type { get; set; }
 	}
